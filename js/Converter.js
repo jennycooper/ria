3911203887eb984a77cf03e96a8b646cbf14dc78
jenny cooper <jennycooper@eircom.net>
2014-03-03 19:44:39 +0000

    first commit

---
+++ js/Converter.js
@@ -0,0 +1,26 @@
+﻿
+var Converter = (function () {
+    // private stuff goes here
+
+    return {
+        convertFromImperialToMetric: function (weight) {
+            if (isNaN(parseFloat(weight))) {
+                return "Not a Number";
+            };
+            var answer = weight / 2.2;
+            return answer;
+         
+                
+        },
+
+        convertFromMetricToImperial: function (weight) {
+            if (isNaN(parseFloat(weight))) {
+                return "Not a Number";
+            };
+            var answer = weight * 2.2;
+            return answer;
+        }
+    }
+
+})();
+
---
+++ test/spec/ConverterSpec.js
@@ -0,0 +1,17 @@
+﻿
+describe("My Converter", function () {
+    it("should convert imperial weight to metric", function () {
+        expect(Converter.convertFromImperialToMetric(2.2)).toBe(1);
+        expect(Converter.convertFromImperialToMetric(4.4)).toBe(2);
+    });
+
+    it("should convert metric weight to imperial", function () {
+        expect(Converter.convertFromMetricToImperial(1)).toBe(2.2);
+    });
+
+    it("should throw the appropriate error when a string is passed in as a param", function () {
+        expect(Converter.convertFromImperialToMetric("hello")).toBe("Not a Number");
+        expect(Converter.convertFromMetricToImperial("hello")).toBe("Not a Number");
+    });
+
+});
